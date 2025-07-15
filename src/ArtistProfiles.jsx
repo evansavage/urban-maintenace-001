a9e89c7e9f8b2ee4cc9f0102234a46d19c69bfa6
evansavage <esav678@gmail.com>
2025-07-15 13:25:38 -0400

    shange speedington of glitch

--- src/ArtistProfiles.jsx
+++ src/ArtistProfiles.jsx
@@ -1,4 +1,4 @@
-export const getRandomInt = (min = 40, max = 200) => {
+export const getRandomInt = (min = 80, max = 400) => {
   min = Math.ceil(min); // Round up the min
   max = Math.floor(max); // Round down the max
   return Math.floor(Math.random() * (max - min + 1)) + min;
